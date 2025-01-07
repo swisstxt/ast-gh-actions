d990d108f2bb1bf718c772a529fd9ca227b77e59
Marco Cadetg <marco.cadetg@swisstxt.ch>
2025-01-07 15:42:41 +0100

    ci tools, husky, lint-staged, eslint config, package.json

---
+++ upstream-tag-on-merge/eslint.config.js
@@ -0,0 +1,19 @@
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default [
+  {
+    ignores: ["eslint.config.js"],
+  },
+  js.configs.recommended,
+  ...tseslint.configs.strictTypeChecked,
+  ...tseslint.configs.stylisticTypeChecked,
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+];
